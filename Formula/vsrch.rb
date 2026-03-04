cc2a64658368394d7fb5b69f3805fb6e8a6c8859
jeanboy <eesidoro44@gmail.com>
2026-03-04 22:35:13 +0900

    chore: update vsrch formula to v0.1.1

--- Formula/vsrch.rb
+++ Formula/vsrch.rb
@@ -5,12 +5,12 @@ class Vsrch < Formula
   version "v0.1.1"
 
   on_macos do
-    url "https://github.com/jeanboy44/vsrch/releases/download/v0.1.1/vsrch-v0.1.1-macos-latest-20260304222931-3b1aad1.tar.gz"
+    url "https://github.com/jeanboy44/vsrch-release/releases/download/v0.1.1/vsrch-v0.1.1-macos-latest-20260304222931-3b1aad1.tar.gz"
     sha256 "a457e5bbef4ceca011dde95316c16f6cf06e929a20a7f60471c6d9516b828da4"
   end
 
   on_linux do
-    url "https://github.com/jeanboy44/vsrch/releases/download/v0.1.1/vsrch-v0.1.1-ubuntu-latest-20260304222939-3b1aad1.tar.gz"
+    url "https://github.com/jeanboy44/vsrch-release/releases/download/v0.1.1/vsrch-v0.1.1-ubuntu-latest-20260304222939-3b1aad1.tar.gz"
     sha256 "8004676800e0ecba14a0b42da3632ac836a71b662839171ee7bae8e1b42dfbb6"
   end
 
